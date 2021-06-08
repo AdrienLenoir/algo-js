e3d92cad59d1125aaa351a356394558cf4ed1488
Adrien Lenoir <lenoiradrien1@gmail.com>
2021-06-08 12:11:16 +0200

    Exercice 4.2

---
+++ 4.2.js
@@ -0,0 +1,10 @@
+// --- Ex. 4.2
+
+// retourne un nombre aléatoire compris entre 1 et 10
+function rand10() {
+    return Math.trunc(Math.random() * 10) + 1
+}
+
+console.log("Random entre 1 et 10 : " + rand10())
+
+
