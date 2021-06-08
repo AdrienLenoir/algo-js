6907b110a2ae878459ca807e85e51eae0e907aca
Adrien Lenoir <lenoiradrien1@gmail.com>
2021-06-08 11:53:07 +0200

    Exercice 2.3

---
+++ 2.3.js
@@ -0,0 +1,13 @@
+// --- Ex. 2.3
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0)
+        console.log(i)
+}
+
+let i = 1
+while (i <= 50) {
+    if (i % 2 === 0)
+        console.log(i)
+    i++
+}
